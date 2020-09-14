65a9770188fef94d62df66caec8bb8a8ceaaa211
Patricio <graded.sp@gmail.com>
2020-09-14 11:04:02 -0300

    markdown logo. start.sh modify

--- startApp.sh
+++ startApp.sh
@@ -1,2 +1,7 @@
 export GO_ENV=production
-buffalo dev
+export HOST="https://curso.whittileaks.com"
+export ADDR=127.0.0.1
+export PORT=3000
+docker restart forum
+
+buffalo dev &>> ./assets/logs/curso.log & # the last ampersand pushes job to background
